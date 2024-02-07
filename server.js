66b808c44ae6dcb1d70e2e26ec2484ffdfff7c26
Varun Patodia <58364635+vrun1208@users.noreply.github.com>
2024-02-07 17:22:37 +0530

    array-length-[fix]

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // const corsOptions = {
 //   origin: process.env.ALLOWED_CLIENTS.split(','),
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Initialize physioAvailability with default values
-let physioAvailability = Array(2).fill().map(() => Array(7).fill().map(() => Array(12).fill({state: 'vacant'})));
+let physioAvailability = Array(2).fill().map(() => Array(7).fill().map(() => Array(24).fill({state: 'vacant'})));
 
 app.get('/api/availability', (req, res) => {
   res.json(physioAvailability);
